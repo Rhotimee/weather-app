07f1d0a387d3aece6a9b95a97708b4d72476ee3d
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2017-12-24 00:45:47 +0100

    prettifying the printed object data

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const request = require('request');
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=23%20marina%20lagos%20nigeria',
-    json: true // this coverts the json to string
+    json: true // this tells javascript that its a json file
 }, (error, response, body) =>{
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2)); // we use stringify to format the object to string
 
 })
