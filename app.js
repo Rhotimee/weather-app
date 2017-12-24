2402714d19f2d7eb111c67d4846e46c6d1842168
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2017-12-24 01:29:30 +0100

    encoding user input & getting the address, lng & lat.

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
+
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=23%20marina%20lagos%20nigeria',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true // this tells javascript that its a json file
 }, (error, response, body) =>{
-    console.log(JSON.stringify(body, undefined, 2)); // we use stringify to format the object to string
-
+    // console.log(JSON.stringify(body, undefined, 2)); // we use stringify to format the object to string
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Address: ${body.results[0].geometry.location.lat}`)
+    console.log(`Address: ${body.results[0].geometry.location.lng}`)
 })
