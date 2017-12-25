fa55b1be9330f3ea137ddb0f883b6fb7d9a03ae5
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2017-12-25 15:33:03 +0100

    callback error

--- app.js
+++ app.js
@@ -21,8 +21,15 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true // this tells javascript that its a json file
 }, (error, response, body) =>{
-    // console.log(JSON.stringify(body, undefined, 2)); // we use stringify to format the object to string
-    console.log(`Address: ${body.results[0].formatted_address}`)
-    console.log(`Address: ${body.results[0].geometry.location.lat}`)
-    console.log(`Address: ${body.results[0].geometry.location.lng}`)
-})
+    if (error){
+        console.log(`Unable to connect to google servers.`);
+    } else if (body.status === 'ZERO_RESULTS'){
+        console.log(`Unable to find that address.`);
+    } else if (body.status === 'OK'){
+        // console.log(JSON.stringify(body, undefined, 2)); // we use stringify to format the object to string
+        console.log(`Address: ${body.results[0].formatted_address}`)
+        console.log(`lat: ${body.results[0].geometry.location.lat}`)
+        console.log(`lng: ${body.results[0].geometry.location.lng}`)
+    }
+
+});
